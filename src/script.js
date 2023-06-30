1e8b4140b3ef78b747e0bf784db7b89ff6fba73f
vasearar <92268205+vasearar@users.noreply.github.com>
2023-06-30 16:18:32 +0300

    Add files via upload

--- src/script.js
+++ src/script.js
@@ -1,4 +1,13 @@
 const cursor = document.querySelector("#cursorFollow");
+const frame = document.querySelector("#frame");
+
+frame.addEventListener("mouseenter", () => {
+  cursor.style.opacity = "0";
+});
+
+frame.addEventListener("mouseleave", () => {
+  cursor.style.opacity = "1";
+});
 
 document.addEventListener("mousemove", (e) => {
   cursor.style.display = "inherit";
@@ -35,12 +44,24 @@ function fadeOutAndFadeInNextImage() {
 setInterval(fadeOutAndFadeInNextImage, 5000);
 
 const greenRectangle = document.querySelector("#greenRectangle");
+const greenH1 = document.querySelector("#greenH1");
+const greenP = document.querySelector("#greenP");
+const greenBtn = document.querySelector("#grnBtn");
 
 window.addEventListener("load", () => {
   greenRectangle.classList.add("toScreen");
   setTimeout(() => {
     greenRectangle.style.transform = "translateX(0%)";
+    greenH1.style.transform = "translateX(0%)";
+    setTimeout(() => {
+      greenP.style.transform = "translateX(0%)";
+      setTimeout(() =>{
+        greenBtn.style.transform = "translateX(0%)";
+      }, 500)
+    }, 500)
+    
   }, 1000)
+  
 });
 
 function redirect() {
@@ -95,16 +116,49 @@ docLines.forEach(line => lineObserver.observe(line));
 
 
 const number = document.querySelector("#numbers");
+const lanuri = document.querySelectorAll(".lan");
+
+lanuri[0].style.transform = "translateX(140%) scale(1.5)";
+lanuri[1].style.transform = "translateX(140%) scale(1.5)";
+
+let i = 2;
+let j = 1;
+let x = 0;
+
 let ratio = -135;
 
-setInterval(() =>{
-  number.classList.add('active');
-  number.style.transform = `translateY(${ratio}px)`;
-  ratio-= 135;
-  if (ratio < -540){
-    number.classList.remove('active');
-    number.style.transform = `translateY(0px)`;
-    ratio = -135;
-  }
-} ,1500);
+let interval = setInterval(() =>{
+  // number.classList.add('active');
+  // number.style.transform = `translateY(${ratio}px)`;
+  // ratio-= 135;
 
+  lanuri[i].style.transform = "scale(1.5)";
+  lanuri[j].style.transform = "scale(1)";
+
+
+  setTimeout(() => {
+    lanuri[i].style.transform = "translateX(140%) scale(1.5)";
+    setTimeout(()=>{
+      lanuri[i].style.zIndex = "10";
+      lanuri[j].style.zIndex = "1";
+      [i, j, x] = [j, x, i];
+    }, 1000);
+  },2000);
+
+
+  // if (ratio < -540){
+  //   number.classList.remove('active');
+  //   number.style.transform = `translateY(0px)`;
+  //   ratio = -135;
+  // }
+} ,3500);
+
+
+const scrolling = document.querySelector("#boxSelf");
+const coordonate = document.querySelector("#hiddenLine");
+
+window.addEventListener("scroll", () => {
+  let cord = coordonate.offsetTop;
+  scrolling.style.transform = `translateX(-${cord}px)`;
+  // console.log(cord);
+});
